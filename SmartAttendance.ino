a2a394fef215cfdf32fc2dce9526d869c2a479e8
JARVIS\Sinclair <exclass9.24@gmail.com>
2018-11-14 13:00:30 +0200

    feat: fire alarm system

--- SmartAttendance.ino
+++ SmartAttendance.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+// Use defines for easier debugging
+#define smokePin 0
+#define buzzerPin 7
 
+int fireThreshold = 150;
+
+void setup() {
+  // NOTE: USE THIS WHEN YOU WANT TO DEBUG, 
+  // THEN USE Serial.println() to print and use the serial monitor from Tools -> Serial Monitor 
+  // Serial.begin(9600); 
+
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(analogRead(smokePin) >= fireThreshold){
+    tone(buzzerPin, 1000);
+  } else {
+    noTone(buzzerPin);
+  }
 }
